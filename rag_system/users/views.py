8240d87bafb7e58892b49680cfa71ee960e2989e
Kuldeep Singh <kuldeep.singh_cs.aiml24@gla.ac.in>
2026-03-29 00:59:09 +0530

    Updating the views of the users app

--- rag_system/users/views.py
+++ rag_system/users/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
+from django.shortcuts import render, redirect
 
 # ---------------- LOGIN ----------------
 def login_view(request):
